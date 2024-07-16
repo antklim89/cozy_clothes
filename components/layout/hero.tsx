b3841ebb92e09b76c3fe1b25edc7b45b61e03700
Anton <antklim89@mail.ru>
2024-07-16 21:13:15 +0300

    feat: update hero style

--- components/layout/hero.tsx
+++ components/layout/hero.tsx
@@ -7,25 +7,27 @@ async function Hero() {
   const { imagePreview, text } = await heroLoader();
 
   return (
-    <section className="relative flex flex-col py-16 lg:pt-0 lg:flex-col lg:pb-0">
-      <div className="flex flex-col items-start w-full max-w-xl px-4 mx-auto lg:px-8 lg:max-w-screen-xl">
-        <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-          <div className="max-w-xl mb-6">
-            <Markdown className="prose">{text}</Markdown>
-          </div>
-          <div className="flex flex-col items-center md:flex-row">
-            <Button>Show more</Button>
+    <section className="bg-slate-100">
+      <div className="container relative flex flex-col py-16 lg:pt-0 lg:flex-col lg:pb-0">
+        <div className="flex flex-col items-start w-full max-w-xl px-4 mx-auto lg:px-8 lg:max-w-screen-xl">
+          <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
+            <div className="max-w-xl mb-6">
+              <Markdown className="prose md:p-1">{text}</Markdown>
+            </div>
+            <div className="flex flex-col items-center md:flex-row">
+              <Button>Show more</Button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="inset-y-0 right-0 w-full max-w-xl px-4 mx-auto lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
-        <Image
-          className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full"
-          src={imagePreview}
-          alt="hero"
-          width={480}
-          height={720}
-        />
+        <div className="inset-y-0 right-0 w-full max-w-xl px-4 mx-auto lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
+          <Image
+            className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full"
+            src={imagePreview}
+            alt="hero"
+            width={480}
+            height={720}
+          />
+        </div>
       </div>
     </section>
   );
