762149af331d301a221a9910e0d6ad0d29d4798c
Anton <antklim89@mail.ru>
2024-07-18 19:36:25 +0300

    feat: add product card

---
+++ components/layout/product-card.tsx
@@ -0,0 +1,30 @@
+import { Button, Card, CardFooter, CardHeader, CardTitle, Price } from '@/components/ui';
+import type { ProductType } from '@/lib';
+import Image from 'next/image';
+
+interface Props {
+  product: ProductType;
+}
+
+function ProductCard({ product }: Props) {
+  return (
+    <Card className="prose prose-h3:my-2 flex flex-col">
+      <CardHeader>
+        <CardTitle>{product.title}</CardTitle>
+        <Image
+          className="w-full h-64 object-contain"
+          src={product.imagePreview}
+          width={480}
+          height={200}
+          alt={product.title}
+        />
+      </CardHeader>
+      <CardFooter className="flex flex-col justify-end items-end">
+        <Price price={product.price} discount={product.discount} />
+        <Button>Show</Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default ProductCard;
