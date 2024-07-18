95c74370e19809117bfbc91baedd0b3bdc81d9fa
Anton <antklim89@mail.ru>
2024-07-18 20:17:15 +0300

    chore: move components to feature

---
+++ components/feature/index.ts
@@ -0,0 +1,6 @@
+
+
+// @ index generate start
+export { default as Product } from './product';
+export { default as ProductCard } from './product-card';
+export { default as ProductsList } from './products-list';
--- components/layout/index.ts
+++ components/layout/index.ts
@@ -1,7 +1,3 @@
-
-
 // @ index generate start
 export { default as Header } from './header';
 export { default as Hero } from './hero';
-export { default as ProductCard } from './product-card';
-export { default as ProductsList } from './products-list';
