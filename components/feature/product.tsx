af3671497f48d3a7dc5fce130dd075e185e2fa11
Anton <antklim89@mail.ru>
2024-07-21 15:02:28 +0300

    feat: add ProductOptions

--- components/feature/product.tsx
+++ components/feature/product.tsx
@@ -1,4 +1,8 @@
+import { ProductOptions } from '@/components/form';
+import { Button, Price } from '@/components/ui';
+import Carousel from '@/components/ui/carousel';
 import type { ProductType } from '@/lib';
+import Image from 'next/image';
 
 interface Props {
   product: ProductType;
@@ -6,9 +10,29 @@ interface Props {
 
 function Product({ product }: Props) {
   return (
-    <div>
-      <section>SECTION</section>
-      <aside>ASIDE</aside>
+    <div className="container flex flex-col md:flex-row">
+      <section className="border px-4 flex-[2_1_0]">
+        <Carousel key={product.id} autoplay={false} scrollDistance="slide" showArrows swiping keyboard>
+          {product.images.map((image) => (
+            <Image
+              key={image}
+              src={image}
+              width={480}
+              height={320}
+              alt={product.title}
+              className="object-cover w-fit h-[28rem] md:h-[48rem]"
+            />
+          ))}
+        </Carousel>
+      </section>
+      <aside className="border prose px-4 flex-[1_1_0] flex flex-col">
+        <h2>{product.title}</h2>
+        <h3>{product.category}</h3>
+        <p>{product.description}</p>
+        <ProductOptions options={product.options} />
+        <Price price={product.price} discount={product.discount} />
+        <Button>Add to cart</Button>
+      </aside>
     </div>
   );
 }
