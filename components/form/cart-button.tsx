6a8a03f4c86050ae981b47fe1f5624e77ca4ebb0
Anton <antklim89@mail.ru>
2024-07-21 21:06:03 +0300

    feat: update cart

--- components/form/cart-button.tsx
+++ components/form/cart-button.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui';
 import type { ProductType } from '@/lib/schemas';
 import { type CartItem, useCartStore } from '@/lib/store';
 import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 import {} from 'zustand/shallow';
 
 interface Props {
@@ -11,8 +12,10 @@ interface Props {
 
 function CartButton({ product }: Props) {
   const searchParams = useSearchParams();
+
   const addToCart = useCartStore((store) => store.addToCart);
   const removeFromCart = useCartStore((store) => store.removeFromCart);
+  const updateCart = useCartStore((store) => store.updateCart);
   const hasCartItem = useCartStore((store) => store.cartItems.findIndex((i) => i.product.id === product.id) >= 0);
 
   const handleAddToCart = () => {
@@ -28,6 +31,15 @@ function CartButton({ product }: Props) {
   const handleRemoveFromCart = () => {
     removeFromCart(product.id);
   };
+
+  useEffect(() => {
+    if (!hasCartItem) return;
+    updateCart(product.id, {
+      qty: Number(searchParams.get('qty') ?? 1),
+      size: searchParams.get('size'),
+      color: searchParams.get('color'),
+    });
+  }, [product.id, searchParams, updateCart, hasCartItem]);
 
   if (hasCartItem) return <Button onClick={handleRemoveFromCart}>Remove From Cart</Button>;
   return <Button onClick={handleAddToCart}>Add To Cart</Button>;
