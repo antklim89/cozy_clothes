ae85de7dd67e17f1b63924f8c62974edf8c05488
Anton <antklim89@mail.ru>
2024-07-21 16:19:09 +0300

    update product options

--- components/form/product-options.tsx
+++ components/form/product-options.tsx
@@ -1,5 +1,8 @@
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui';
-import { type ProductType, cn } from '@/lib';
+'use client';
+import { FormItem, Label, Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui';
+import type { ProductType } from '@/lib/schemas';
+import { cn } from '@/lib/utils';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import type { ComponentProps } from 'react';
 
 type Props = ComponentProps<'form'> & {
@@ -7,32 +10,52 @@ type Props = ComponentProps<'form'> & {
 };
 
 function ProductOptions({ options, className, ...props }: Props) {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const handleValueChange = (field: string) => (value: string) => {
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set(field, value);
+    replace(`${pathname}?${newSearchParams}`);
+  };
+
   return (
     <form {...props} className={cn('flex flex-col gap-4', className)}>
-      <Select>
-        <SelectTrigger>
-          <SelectValue placeholder="Sizes" />
-        </SelectTrigger>
-        <SelectContent>
-          {options.sizes.map((size) => (
-            <SelectItem key={size} value={size}>
-              {size}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-      <Select>
-        <SelectTrigger>
-          <SelectValue placeholder="Colors" />
-        </SelectTrigger>
-        <SelectContent>
-          {options.colors.map((color) => (
-            <SelectItem key={color} value={color}>
-              {color}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
+      <FormItem>
+        <Label htmlFor="size">Size:</Label>
+        <Select onValueChange={handleValueChange('size')} defaultValue={searchParams.get('size') ?? options.sizes[0]}>
+          <SelectTrigger className="uppercase" id="size">
+            <SelectValue placeholder="Select a size..." />
+          </SelectTrigger>
+          <SelectContent>
+            {options.sizes.map((size) => (
+              <SelectItem className="uppercase" key={size} value={size}>
+                {size}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </FormItem>
+
+      <FormItem>
+        <Label htmlFor="color">Color:</Label>
+        <Select
+          onValueChange={handleValueChange('color')}
+          defaultValue={searchParams.get('color') ?? options.colors[0]}
+        >
+          <SelectTrigger className="uppercase" id="color">
+            <SelectValue placeholder="Select a color..." />
+          </SelectTrigger>
+          <SelectContent>
+            {options.colors.map((color) => (
+              <SelectItem className="uppercase" key={color} value={color}>
+                {color}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </FormItem>
     </form>
   );
 }
