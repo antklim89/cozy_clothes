7e251498d2e33e268b02553f1b911cf2dedf4c76
Anton <antklim89@mail.ru>
2024-07-24 22:05:17 +0300

    feat: move get price to separate util function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+const priceFormat = Intl.NumberFormat(undefined, {
+  maximumFractionDigits: 2,
+  minimumFractionDigits: 2,
+  currency: 'USD',
+  style: 'currency',
+  currencyDisplay: 'symbol',
+  useGrouping: false,
+});
+
+export function getPrice(price: number, discount = 0): string {
+  if (discount <= 0) return priceFormat.format(price);
+  return priceFormat.format(price - price * (discount / 100));
 }
