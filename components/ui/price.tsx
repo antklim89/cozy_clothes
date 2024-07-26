8132c7b2d66550d9124c3b9977a726b9792333f9
Anton <antklim89@mail.ru>
2024-07-26 19:43:27 +0300

    change text size

--- components/ui/price.tsx
+++ components/ui/price.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 function Price({ price, discount = 0 }: Props) {
   return (
-    <p className="flex flex-col items-end gap-x-1">
-      <span className="text-4xl font-bold tracking-tight text-gray-900">{getPrice({ price, discount })}</span>
-      {discount > 0 && <span className="line-through text-xl font-sans text-gray-500/70">{getPrice({ price })}</span>}
+    <p className="flex flex-col items-end gap-1">
+      <span className="text-xl font-bold tracking-tight text-gray-900">{getPrice({ price, discount })}</span>
+      {discount > 0 && <span className="line-through text-md font-sans text-gray-500/70">{getPrice({ price })}</span>}
     </p>
   );
 }
