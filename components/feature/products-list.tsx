0eb9840c09d1a20c2bee72a25b7c0d2252f9e1f3
Anton <antklim89@mail.ru>
2024-07-26 19:44:27 +0300

    refactor: change product list grid style

--- components/feature/products-list.tsx
+++ components/feature/products-list.tsx
@@ -14,7 +14,10 @@ async function ProductsList({ skip, limit, ...props }: Props) {
   return (
     <div
       {...props}
-      className={cn('container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2', props.className)}
+      className={cn(
+        'container grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"',
+        props.className,
+      )}
     >
       {products.slice(skip, limit).map((product) => (
         <ProductCard key={product.id} product={product} />
