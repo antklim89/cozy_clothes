a35f927fb096b2171e81fac2fee919a54ff16539
Anton <antklim89@mail.ru>
2024-07-26 19:43:51 +0300

    refactor: update product card

--- components/feature/product-card.tsx
+++ components/feature/product-card.tsx
@@ -1,7 +1,6 @@
-import { Button, Card, CardFooter, CardHeader, CardTitle, Price } from '@/components/ui';
 import type { ProductType } from '@/lib/schemas';
 import Image from 'next/image';
-import Link from 'next/link';
+import { Price } from '../ui';
 
 interface Props {
   product: ProductType;
@@ -9,26 +8,29 @@ interface Props {
 
 function ProductCard({ product }: Props) {
   return (
-    <Card className="prose prose-h3:my-2 flex flex-col">
-      <CardHeader>
-        <CardTitle>{product.title}</CardTitle>
+    <div key={product.id} className="group relative">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
         <Image
-          className="w-full h-64 object-contain"
-          src={product.imagePreview}
-          width={480}
-          height={200}
+          width={400}
+          height={400}
           alt={product.title}
+          src={product.imagePreview}
+          className="h-72 w-full object-cover object-center lg:h-80 lg:w-full"
         />
-      </CardHeader>
-      <CardFooter className="flex flex-col justify-end items-end">
+      </div>
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h3 className="text-sm text-gray-700">
+            <a href={`/product/${product.id}`}>
+              <span aria-hidden="true" className="absolute inset-0" />
+              {product.title}
+            </a>
+          </h3>
+          <p className="mt-1 text-sm text-gray-500">{product.category}</p>
+        </div>
         <Price price={product.price} discount={product.discount} />
-        <Button asChild>
-          <Link className="no-underline" href={`/product/${product.id}`}>
-            Show
-          </Link>
-        </Button>
-      </CardFooter>
-    </Card>
+      </div>
+    </div>
   );
 }
 
