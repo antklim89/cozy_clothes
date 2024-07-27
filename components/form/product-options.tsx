f22c08545d0f0587aed80c8a15130caf054984fd
Anton <antklim89@mail.ru>
2024-07-27 19:28:03 +0300

    update events

--- components/form/product-options.tsx
+++ components/form/product-options.tsx
@@ -1,36 +1,25 @@
 'use client';
 import { FormItem, Input, Label, ToggleGroup, ToggleGroupItem } from '@/components/ui';
-import type { ProductType } from '@/lib/schemas';
+import { type ProductType, qtySchema } from '@/lib/schemas';
 import { cn } from '@/lib/utils';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import type { ChangeEvent, ComponentProps } from 'react';
+import type { ComponentProps } from 'react';
 
 type Props = ComponentProps<'form'> & {
   options: ProductType['options'];
 };
-
-const MAX_QTY = 100;
-const MIN_QTY = 1;
 
 function ProductOptions({ options, className, ...props }: Props) {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const pathname = usePathname();
 
-  const handleValueChange = (field: string) => (value: string) => {
+  const handleValueChange = (field: string, value: string | number) => {
     const newSearchParams = new URLSearchParams(searchParams);
-    newSearchParams.set(field, value);
+    newSearchParams.set(field, value.toString());
     replace(`${pathname}?${newSearchParams}`, { scroll: false });
   };
 
-  const handleQtyChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const qty = Math.max(MIN_QTY, Math.min(MAX_QTY, e.target.valueAsNumber));
-    const newSearchParams = new URLSearchParams(searchParams);
-    newSearchParams.set('qty', Number.isNaN(qty) ? '1' : qty.toString());
-    replace(`${pathname}?${newSearchParams}`, { scroll: false });
-  };
-
-  const selectedColor = searchParams.get('color') ?? options.colors?.[0];
   return (
     <form {...props} className={cn('flex flex-col gap-4', className)}>
       {options.sizes && (
@@ -39,7 +28,7 @@ function ProductOptions({ options, className, ...props }: Props) {
           <ToggleGroup
             type="single"
             value={searchParams.get('size') ?? options.sizes[0]}
-            onValueChange={handleValueChange('size')}
+            onValueChange={(value) => handleValueChange('size', value)}
           >
             {options.sizes.map((size) => (
               <ToggleGroupItem key={size} value={size}>
@@ -53,7 +42,11 @@ function ProductOptions({ options, className, ...props }: Props) {
       {options.colors && (
         <FormItem>
           <Label htmlFor="color">Color:</Label>
-          <ToggleGroup type="single" value={selectedColor} onValueChange={handleValueChange('color')}>
+          <ToggleGroup
+            type="single"
+            value={searchParams.get('color') ?? options.colors[0]}
+            onValueChange={(value) => handleValueChange('color', value)}
+          >
             {options.colors.map((color) => (
               <ToggleGroupItem key={color} value={color} className="flex items-center relative overflow-hidden">
                 <div
@@ -71,9 +64,10 @@ function ProductOptions({ options, className, ...props }: Props) {
         <Label htmlFor="color">Quantity:</Label>
         <Input
           type="number"
-          min={MIN_QTY}
-          max={MAX_QTY}
-          onChange={handleQtyChange}
+          min={1}
+          max={100}
+          onChange={(e) => handleValueChange('qty', e.target.value)}
+          onBlur={(e) => handleValueChange('qty', qtySchema.parse(e.target.valueAsNumber))}
           value={searchParams.get('qty') ?? '1'}
         />
       </FormItem>
