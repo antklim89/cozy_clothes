73aeb977f8dc5aa9b3fc6574f93056b15343a798
Anton <antklim89@mail.ru>
2024-07-29 20:42:22 +0300

    feat(components): add filter by page and category

--- components/feature/products-list.tsx
+++ components/feature/products-list.tsx
@@ -4,22 +4,30 @@ import type { ComponentProps } from 'react';
 import ProductCard from './product-card';
 
 interface Props extends ComponentProps<'div'> {
+  page?: number;
+  category?: string;
   limit?: number;
-  skip?: number;
 }
 
-async function ProductsList({ skip, limit, ...props }: Props) {
-  const products = await productsLoader();
+const ALL_CATEGORIES = 'all';
+
+async function ProductsList({ limit = 8, page = 1, category = ALL_CATEGORIES, className, ...props }: Props) {
+  let products = await productsLoader();
+
+  if (category !== ALL_CATEGORIES) {
+    products = products.filter((i) => i.category === category);
+  }
+
+  const start = limit * page - limit;
+  const end = start + limit;
+  products = products.slice(start, end);
 
   return (
     <div
       {...props}
-      className={cn(
-        'container grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"',
-        props.className,
-      )}
+      className={cn('container grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"', className)}
     >
-      {products.slice(skip, limit).map((product) => (
+      {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
     </div>
