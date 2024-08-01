d3dac81e8e1b4dfaf042b6b1460ac30b11390f9e
Anton <antklim89@mail.ru>
2024-08-01 21:33:24 +0300

    feat(components): add discount sorted products

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,20 @@ import { CategoriesNavBar, Hero } from '@/components/layout';
 import { productsLoader } from '@/lib/contentLoaders';
 
 const HomePage = async () => {
-  const products = (await productsLoader()).slice(0, 8);
+  const products = await productsLoader();
+
+  const newSortedProducts = products.slice(0, 8);
+  const discountSortedProducts = products.toSorted((a, b) => b.discount - a.discount).slice(0, 8);
 
   return (
     <div>
       <Hero />
       <CategoriesNavBar className="my-4" />
       <h2 className="prose text-center text-2xl mt-4 font-bold">New products</h2>
-      <ProductsList products={products} className="my-8" />
+      <ProductsList products={newSortedProducts} className="my-8" />
       <Testimonials />
+      <h2 className="prose text-center text-2xl mt-4 font-bold">Big discounts</h2>
+      <ProductsList products={discountSortedProducts} className="my-8" />
     </div>
   );
 };
