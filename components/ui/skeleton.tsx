02e1a28461afde9853ffc79691a97b004e1e2b4a
Anton <antklim89@mail.ru>
2024-08-11 19:51:28 +0300

    feat(components): add SkeletonText

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -5,4 +5,14 @@ function Skeleton({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
   return <div className={cn('animate-pulse rounded-md bg-muted', className)} {...props} />;
 }
 
-export { Skeleton };
+function SkeletonText({ className, number = 2, ...props }: HTMLAttributes<HTMLDivElement> & { number?: number }) {
+  return (
+    <>
+      {Array.from({ length: number }, (_, i) => i).map((i) => (
+        <Skeleton key={i} className={cn('w-full h-4 mb-2', className)} {...props} />
+      ))}
+    </>
+  );
+}
+
+export { Skeleton, SkeletonText };
