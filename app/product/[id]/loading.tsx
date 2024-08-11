013d723a7cf433522ed3e1c029ac2bd65a26fa83
Anton <antklim89@mail.ru>
2024-08-11 19:09:42 +0300

    feat(app): add product loading

---
+++ app/product/[id]/loading.tsx
@@ -0,0 +1,12 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
+const ProductLoading = () => {
+  return (
+    <div className="container grid grid-cols-[2fr_1fr] gap-4 my-8">
+      <Skeleton className="h-[80dvh] rounded-sm" />
+      <Skeleton className="h-[80dvh] rounded-sm" />
+    </div>
+  );
+};
+
+export default ProductLoading;
