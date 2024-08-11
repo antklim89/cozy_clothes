d0b3a136b45116ceb6cd36464f6408a3388ebb07
Anton <antklim89@mail.ru>
2024-08-11 18:44:13 +0300

    feat(constants): add defaultBlurDataUrl

--- constants.ts
+++ constants.ts
@@ -1,2 +1,4 @@
 export const PRODUCTS_PER_PAGE = 8;
 export const ALL_CATEGORIES = 'all';
+export const defaultBlurDataUrl =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8MlGqHgAFwAIAnnxXJwAAAABJRU5ErkJggg==';
