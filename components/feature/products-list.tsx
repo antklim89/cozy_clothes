057e58b859dc3c07686fb5a0bb9dcad7f481fa2f
Anton <antklim89@mail.ru>
2024-08-15 19:25:06 +0300

    fix: remove async

--- components/feature/products-list.tsx
+++ components/feature/products-list.tsx
@@ -7,7 +7,7 @@ interface Props extends ComponentProps<'div'> {
   products: ProductType[];
 }
 
-export const ProductsList = async ({ products, className, ...props }: Props) => {
+export const ProductsList = ({ products, className, ...props }: Props) => {
   return (
     <div
       {...props}
