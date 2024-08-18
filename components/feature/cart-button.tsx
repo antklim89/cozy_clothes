757fad7c41c15f461c6beb8308f2964990c0eec7
Anton <antklim89@mail.ru>
2024-08-18 13:39:09 +0300

    fix: check cartItems length

--- components/feature/cart-button.tsx
+++ components/feature/cart-button.tsx
@@ -18,24 +18,26 @@ export const CartButton = ({ className, ...props }: ComponentProps<'a'>) => {
           <p className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">{cartItems.length}</p>
         </Link>
       </HoverCardTrigger>
-      <HoverCardContent className="min-w-80 p-1 flex flex-col gap-1">
-        {cartItems.map(({ id, product, qty, color, size }) => (
-          <Card key={id}>
-            <CardContent className="relative">
-              <p className="text-md mb-2">
-                {product.title} x {qty}
-              </p>
-              <div className="flex gap-2">
-                <span className="text-sm text-gray-500 border p-1 uppercase">{size}</span>
-                <span className="text-sm text-gray-500 border p-1 uppercase">{color}</span>
-              </div>
-              <Link href={`/product/${product.id}?qty=${qty}&size=${size}&color=${color}`}>
-                <span aria-hidden="true" className="absolute inset-0" />
-              </Link>
-            </CardContent>
-          </Card>
-        ))}
-      </HoverCardContent>
+      {cartItems.length > 0 ? (
+        <HoverCardContent className="min-w-80 p-1 flex flex-col gap-1">
+          {cartItems.map(({ id, product, qty, color, size }) => (
+            <Card key={id}>
+              <CardContent className="relative">
+                <p className="text-md mb-2">
+                  {product.title} x {qty}
+                </p>
+                <div className="flex gap-2">
+                  <span className="text-sm text-gray-500 border p-1 uppercase">{size}</span>
+                  <span className="text-sm text-gray-500 border p-1 uppercase">{color}</span>
+                </div>
+                <Link href={`/product/${product.id}?qty=${qty}&size=${size}&color=${color}`}>
+                  <span aria-hidden="true" className="absolute inset-0" />
+                </Link>
+              </CardContent>
+            </Card>
+          ))}
+        </HoverCardContent>
+      ) : null}
     </HoverCard>
   );
 };
