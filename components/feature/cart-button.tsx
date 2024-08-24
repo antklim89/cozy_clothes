8c6bb583e5d572447a528a9247dd3e853bdeb1c8
Anton <antklim89@mail.ru>
2024-08-24 21:53:53 +0300

    style(components): remove hover-card , update cart item count style

--- components/feature/cart-button.tsx
+++ components/feature/cart-button.tsx
@@ -1,6 +1,4 @@
 'use client';
-import { Card, CardContent } from '@/components/ui/card';
-import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
 import { useCartStore } from '@/lib/store';
 import { cn } from '@/lib/utils';
 import { ShoppingCart } from 'lucide-react';
@@ -9,40 +7,15 @@ import type { ComponentProps } from 'react';
 
 export const CartButton = ({ className, ...props }: ComponentProps<'a'>) => {
   const cartItems = useCartStore((state) => state.cartItems);
-  const hasTouch = 'ontouchstart' in window || navigator.maxTouchPoints;
 
   return (
-    <HoverCard openDelay={0} open={hasTouch ? false : undefined}>
-      <HoverCardTrigger asChild>
-        <Link href="/checkout" {...props} className={cn('flex flex-nowrap', className)}>
-          <ShoppingCart />
-          <p className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">{cartItems.length}</p>
-        </Link>
-      </HoverCardTrigger>
-      {cartItems.length > 0 ? (
-        <HoverCardContent className="min-w-80 p-1 flex flex-col gap-1">
-          {cartItems.map(({ id, product, color, size }) => {
-            const searchParams = new URLSearchParams();
-            if (size) searchParams.set('size', size.toString());
-            if (color) searchParams.set('color', color.toString());
-
-            return (
-              <Card key={id}>
-                <CardContent className="relative">
-                  <p className="text-md mb-2">{product.title}</p>
-                  <div className="flex gap-2">
-                    {size && <span className="text-sm text-gray-500 border p-1 uppercase">{size}</span>}
-                    {color && <span className="text-sm text-gray-500 border p-1 uppercase">{color}</span>}
-                  </div>
-                  <Link href={`/product/${product.id}?${searchParams.toString()}`}>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                  </Link>
-                </CardContent>
-              </Card>
-            );
-          })}
-        </HoverCardContent>
-      ) : null}
-    </HoverCard>
+    <Link href="/checkout" {...props} className={cn('flex flex-nowrap', className)}>
+      <ShoppingCart />
+      <p className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+        <span className="bg-red-600 rounded-full min-w-4 p-[2px] text-xs flex justify-center items-center -ml-4 -mt-4">
+          {cartItems.length}
+        </span>
+      </p>
+    </Link>
   );
 };
