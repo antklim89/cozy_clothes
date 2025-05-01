3e63462de63f2943a35001be518faa40e0c10d62
Anton <antklim89@mail.ru>
2025-05-01 11:30:16 +0300

    feat: update ErrVariant type, add errMap, okMap

--- lib/result.ts
+++ lib/result.ts
@@ -1,58 +1,60 @@
 
-
-export interface Success<T> {
-  type: 'success';
+export interface Ok<T> {
+  type: 'ok';
   result: T;
-  message: null;
   error: null;
 }
 
-export interface Err {
+export interface Err<T extends string = string> {
   type: 'error';
-  error: {
-    type: null;
-    message: string;
-  } | {
-    type: 'unexpected';
-    message: string;
-  } | {
-    type: 'not-found';
-    message: string;
-  } | {
-    type: 'validation';
-    errors: Record<string, unknown>;
-  };
+  error: ErrVariant<T>;
   result: null;
 }
 
-export type Result<T> = Success<T> | Err;
+interface ErrVariant<T extends string> {
+  type: T;
+  message: string;
+  original?: unknown;
+  errors?: Record<string, string>;
+}
 
-export type PromiseResult<T> = Promise<Result<T>>;
+export type Result<T, E extends string = string> = Ok<T> | Err<E>;
+
+export type PromiseResult<T, E extends string = string> = Promise<Result<T, E>>;
 
 
 export const UNEXPECTED_ERROR = {
   type: 'unexpected',
   message: 'Unexpected error. Try again later.',
-} as const satisfies Err['error'];
+} as const satisfies ErrVariant<'unexpected'>;
 
 export const NOT_FOUND_ERROR = {
   type: 'not-found',
   message: 'Not found.',
-} as const satisfies Err['error'];
+} as const satisfies ErrVariant<'not-found'>;
 
-export function success<T>(result: T): Success<T> {
+export function ok<const T>(result: T): Ok<T> {
   return {
-    type: 'success',
+    type: 'ok',
     result,
-    message: null,
     error: null,
   };
 }
 
-export function err(error: Err['error']): Err {
+export function err<const T extends string>(error: ErrVariant<T>): Err<T> {
   return {
     type: 'error',
     error,
     result: null,
   };
+}
+
+export function errMap<OldErr extends string, NewErr extends string, R>(result: Result<R, OldErr>, errCb: (arg: ErrVariant<OldErr>) => ErrVariant<NewErr>): Result<R, NewErr> {
+  if (result.type === 'ok') return result;
+  return err(errCb(result.error));
+}
+
+export function okMap<R, U, E extends string>(result: Result<R, E>, okCb: (arg: R) => U): Result<U, E> {
+  if (result.type === 'error') return result;
+  return ok(okCb(result.result));
 }
