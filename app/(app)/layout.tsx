a67bfb52c0c9082d067ecfee0ff7a0f4330456be
Anton <antklim89@mail.ru>
2025-05-02 11:05:13 +0300

    feat: add nuqs provider

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,13 +1,14 @@
-import type { Metadata } from 'next';
-import type { ReactNode } from 'react';
 import '@/styles/main.css';
 import '@fontsource/poppins/400-italic.css';
 import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/700-italic.css';
 import '@fontsource/poppins/700.css';
+import type { Metadata } from 'next';
+import type { ReactNode } from 'react';
 import process from 'node:process';
 import { Footer } from '@/components/layout/footer';
 import { Header } from '@/components/layout/header';
+import NuqsProvider from '@/components/providers/nuqs-provider';
 import { QueryProvider } from '@/components/providers/query-provider';
 import { CategoryDropdownMenu } from '@/features/product-categories';
 import { fetchSeo } from '@/features/seo';
@@ -59,13 +60,15 @@ function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head />
-      <QueryProvider>
-        <body className="grid grid-rows-[auto_1fr_auto] h-screen">
-          <Header categoryMenu={<CategoryDropdownMenu />} />
-          <main>{children}</main>
-          <Footer />
-        </body>
-      </QueryProvider>
+      <NuqsProvider>
+        <QueryProvider>
+          <body className="grid grid-rows-[auto_1fr_auto] h-screen">
+            <Header categoryMenu={<CategoryDropdownMenu />} />
+            <main>{children}</main>
+            <Footer />
+          </body>
+        </QueryProvider>
+      </NuqsProvider>
     </html>
   );
 }
