bb00d512be518df76e3cd2d438c214c635a73276
Anton <antklim89@mail.ru>
2025-08-03 16:31:24 +0300

    fix: fetch me only on client

--- src/entities/user/hooks/use-user.ts
+++ src/entities/user/hooks/use-user.ts
@@ -1,16 +1,22 @@
 import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
 import { meQuery } from '../api/queries/me';
+import type { UserType } from '../model';
 
 
-export const userQueryOptions = queryOptions({
+export const userQueryOptions = queryOptions<UserType | null>({
   refetchOnMount: false,
   refetchOnWindowFocus: false,
   refetchOnReconnect: false,
   refetchInterval: false,
   queryKey: ['user'],
   queryFn: async () => {
+    if (typeof window === 'undefined') return null;
     const user = await meQuery();
-    return user;
+    if (!user) return null;
+    return {
+      email: user.email,
+      id: user.id,
+    };
   },
 });
 
