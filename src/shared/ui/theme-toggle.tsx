c18527eab9f027d0b49d884b63822184fe7fb8ff
Anton <antklim89@mail.ru>
2025-08-07 20:32:33 +0300

    feat: update ThemeToggle
    fix link, add props

--- src/shared/ui/theme-toggle.tsx
+++ src/shared/ui/theme-toggle.tsx
@@ -1,19 +1,25 @@
 'use client';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
-import { useIsClient } from '@/hidden/hooks/use-is-client';
+import type { ComponentProps } from 'react';
+import { useIsClient } from '@/src/shared/hooks/use-is-client';
 import { Button } from './button';
 import { Skeleton } from './skeleton';
 
 
-export function ThemeToggle() {
+export function ThemeToggle(props: ComponentProps<typeof Button>) {
   const { setTheme, theme } = useTheme();
 
   const isClient = useIsClient();
   if (!isClient) return <Skeleton className="rounded-full size-8" />;
 
   return (
-    <Button suppressHydrationWarning variant="ghost" onClick={() => theme === 'dark' ? setTheme('light') : setTheme('dark')}>
+    <Button
+      suppressHydrationWarning
+      variant="ghost"
+      {...props}
+      onClick={() => theme === 'dark' ? setTheme('light') : setTheme('dark')}
+    >
       {theme === 'dark' ? <Sun /> : <Moon />}
     </Button>
   );
