4c39cab36fec8aac07435773bcb43e29f1a3df74
Anton <antklim89@mail.ru>
2025-11-10 21:34:53 +0300

    fix: update use debounce and use update cart mutation

--- src/features/update-cart/api/mutations/use-update-cart-mutation.ts
+++ src/features/update-cart/api/mutations/use-update-cart-mutation.ts
@@ -3,24 +3,31 @@ import { useMutation, useQueryClient, useSuspenseQuery } from '@tanstack/react-q
 import { cartQueryOptions } from '@/entities/cart/api';
 import { updateCartQtyInLocalStorage } from '@/entities/cart/lib';
 import { meQueryOptions } from '@/entities/user/api';
+import { useDebounce } from '@/shared/hooks/use-debounce';
 import { updateCartQtyAction } from '../actions';
 
 export function useUpdateCartMutation() {
   const queryClient = useQueryClient();
   const { data: user } = useSuspenseQuery(meQueryOptions);
+  const updateCartQtyActionDebounced = useDebounce(updateCartQtyAction);
 
   return useMutation({
     mutationFn: async (updatedCartItem: { productId: number; qty: number }) => {
       if (user) {
-        const updateCartQtyResult = await updateCartQtyAction(updatedCartItem);
+        const updateCartQtyResult = await updateCartQtyActionDebounced(updatedCartItem);
         if (updateCartQtyResult.type === 'error') throw new Error(updateCartQtyResult.error.message);
       } else {
         updateCartQtyInLocalStorage(updatedCartItem);
       }
     },
-    onSuccess(_data, args) {
+    onMutate(variables) {
       queryClient.setQueryData(cartQueryOptions().queryKey, oldValue =>
-        oldValue?.map(i => (i.productId === args.productId ? { ...i, qty: args.qty } : i)),
+        oldValue?.map(i => (i.productId === variables.productId ? { ...i, qty: variables.qty } : i)),
+      );
+    },
+    onSuccess(_data, variables) {
+      queryClient.setQueryData(cartQueryOptions().queryKey, oldValue =>
+        oldValue?.map(i => (i.productId === variables.productId ? { ...i, qty: variables.qty } : i)),
       );
     },
   });
--- src/shared/hooks/use-debounce.ts
+++ src/shared/hooks/use-debounce.ts
@@ -1,20 +1,20 @@
 import { useCallback, useRef } from 'react';
 
-export function useDebounce<T extends (...args: never[]) => unknown>(callback: T, delay = 700): T {
+export function useDebounce<T extends (...args: never[]) => Promise<unknown>>(callback: T, delay = 700) {
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const debouncedCallback = useCallback(
     (...args: Parameters<T>) => {
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
-      }
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
 
-      timeoutRef.current = setTimeout(() => {
-        callback(...args);
-      }, delay);
+      return new Promise<ReturnType<T>>(resolve => {
+        timeoutRef.current = setTimeout(() => {
+          callback(...args).then(resolve);
+        }, delay);
+      });
     },
     [callback, delay],
-  ) as T;
+  );
 
   return debouncedCallback;
 }
