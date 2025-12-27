cf690ee5465582b41362c5c13a874798b8922604
Anton <antklim89@mail.ru>
2025-12-27 15:30:34 +0300

    fix(styles): add field remove slider

--- src/entities/products/ui/product-price-filter.tsx
+++ src/entities/products/ui/product-price-filter.tsx
@@ -1,59 +1,52 @@
 'use client';
 
 import { parseAsInteger, useQueryStates } from 'nuqs';
+import { z } from 'zod/v4-mini';
 
+import { Field, FieldGroup, FieldLabel } from '@/shared/ui/field';
 import { Input } from '@/shared/ui/input';
-import { Label } from '@/shared/ui/label';
-import { Slider } from '@/shared/ui/slider';
 
 const queryStateOptions = parseAsInteger.withOptions({
   shallow: false,
   limitUrlUpdates: { method: 'debounce', timeMs: 700 },
 });
-const MAX_PRICE = 1000000;
 
 export function PriceFilter() {
   const [price, setPrice] = useQueryStates({
     minPrice: queryStateOptions.withDefault(0),
-    maxPrice: queryStateOptions.withDefault(MAX_PRICE),
+    maxPrice: queryStateOptions.withDefault(0),
     page: parseAsInteger,
   });
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex gap-2">
-        <div className="flex-1">
-          <Label>Min Price</Label>
+      <FieldGroup className="flex flex-row gap-2">
+        <Field className="flex-1">
+          <FieldLabel>Min Price</FieldLabel>
           <Input
             max={price.maxPrice}
             min={0}
-            type="number"
+            type="text"
+            inputMode="numeric"
             value={price.minPrice}
-            onChange={async e => setPrice({ minPrice: Number.parseInt(e.target.value || '0', 10), page: null })}
+            onChange={async e =>
+              setPrice({ minPrice: z.catch(z.coerce.number(), 0).parse(e.target.value), page: null })
+            }
           />
-        </div>
-        <div className="flex-1">
-          <Label>Max Price</Label>
+        </Field>
+        <Field className="flex-1">
+          <FieldLabel>Max Price</FieldLabel>
           <Input
             min={price.minPrice}
-            type="number"
+            type="text"
+            inputMode="numeric"
             value={price.maxPrice}
-            onChange={async e => setPrice({ maxPrice: Number.parseInt(e.target.value || '0', 10), page: null })}
+            onChange={async e =>
+              setPrice({ maxPrice: z.catch(z.coerce.number(), 0).parse(e.target.value), page: null })
+            }
           />
-        </div>
-      </div>
-      <Slider
-        max={MAX_PRICE}
-        min={0}
-        step={10}
-        value={[price.minPrice, price.maxPrice]}
-        onValueChange={value => {
-          if (!Array.isArray(value)) return;
-          const [minValue, maxValue] = value;
-          if (minValue != null) void setPrice({ minPrice: minValue, page: null });
-          if (maxValue != null) void setPrice({ maxPrice: maxValue, page: null });
-        }}
-      />
+        </Field>
+      </FieldGroup>
     </div>
   );
 }
