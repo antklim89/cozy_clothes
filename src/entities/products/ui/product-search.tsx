94d74c043386e9c7e144b5bf11ed8b12b7eecac2
Anton <antklim89@mail.ru>
2025-12-27 15:30:09 +0300

    fix(style): add field

--- src/entities/products/ui/product-search.tsx
+++ src/entities/products/ui/product-search.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { parseAsInteger, parseAsString, useQueryStates } from 'nuqs';
 
+import { Field, FieldLabel } from '@/shared/ui/field';
 import { Input } from '@/shared/ui/input';
-import { Label } from '@/shared/ui/label';
 
 const queryStateOptions = parseAsString
   .withOptions({ shallow: false, limitUrlUpdates: { method: 'debounce', timeMs: 700 } })
@@ -12,9 +12,9 @@ export function ProductSearch() {
   const [searchTerm, setSearchTerm] = useQueryStates({ search: queryStateOptions, page: parseAsInteger });
 
   return (
-    <div>
-      <Label>Search</Label>
+    <Field>
+      <FieldLabel>Search</FieldLabel>
       <Input value={searchTerm.search} onChange={async e => setSearchTerm({ search: e.target.value, page: null })} />
-    </div>
+    </Field>
   );
 }
