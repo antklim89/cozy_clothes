544ecdd9ed9a1f9c001b57c2282dc4f1a21d7180
Anton <antklim89@mail.ru>
2026-01-05 19:41:52 +0300

    fix: remove auth pages

--- src/app/(app)/(auth)/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Auth } from '@/features/auth/ui';
-
-function Page() {
-  return <Auth type="login" />;
-}
-
-export default Page;
--- src/app/(app)/(auth)/register/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Auth } from '@/features/auth/ui';
-
-function Page() {
-  return <Auth type="register" />;
-}
-
-export default Page;
