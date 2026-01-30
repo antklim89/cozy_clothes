df6569a6b37529ee88b017994c250dc65192262a
Anton <antklim89@mail.ru>
2026-01-30 20:10:26 +0300

    feat(ui): update empty cart

--- src/entities/cart/ui/cart-list-empty.tsx
+++ src/entities/cart/ui/cart-list-empty.tsx
@@ -1,21 +1,29 @@
+import { ShoppingBasketIcon } from 'lucide-react';
 import Link from 'next/link';
 
-import { Button } from '@/shared/ui/button';
-import { Card, CardContent, CardHeader } from '@/shared/ui/card';
+import { buttonVariants } from '@/shared/ui/button';
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from '@/shared/ui/empty';
 
 export function CartListEmpty() {
   return (
-    <section className="container my-8">
-      <Card>
-        <CardHeader>
-          <h3 className="text-xl">Shopping cart is empty</h3>
-        </CardHeader>
-        <CardContent>
-          <Button>
-            <Link href="/products">Catalog</Link>
-          </Button>
-        </CardContent>
-      </Card>
-    </section>
+    <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <ShoppingBasketIcon />
+        </EmptyMedia>
+        <EmptyTitle>Cart is empty.</EmptyTitle>
+        <EmptyDescription>
+          You haven&apos;t added any product yet. Get started by browsing our catalog.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent className="flex-row justify-center gap-2">
+        <Link className={buttonVariants({ size: 'lg' })} href="/products">
+          Catalog
+        </Link>
+        <Link className={buttonVariants({ size: 'lg', variant: 'outline' })} href="/">
+          Home
+        </Link>
+      </EmptyContent>
+    </Empty>
   );
 }
