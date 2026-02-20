31d5a51466b512ce85addf2b062c116e7b64089b
Anton <antklim89@mail.ru>
2026-02-20 06:54:35 +0000

    feat: add RatingSelect component

--- src/shared/ui/rating.tsx
+++ src/shared/ui/rating.tsx
@@ -1,5 +1,6 @@
 import { StarIcon } from 'lucide-react';
 
+import { Button } from './button';
 import { cn } from '../lib/utils';
 
 export function Rating({ rating }: { rating: number }) {
@@ -7,7 +8,7 @@ export function Rating({ rating }: { rating: number }) {
     <div className="flex gap-2">
       {Array.from({ length: 5 }, (_, i) => (
         // biome-ignore lint/suspicious/noArrayIndexKey: ok
-        <StarIcon className={cn('stroke-amber-700', { 'fill-amber-600': i + 1 <= rating })} key={i} />
+        <StarIcon className={cn('stroke-amber-700', { 'fill-amber-600': i < rating })} key={i} />
       ))}
     </div>
   );
@@ -19,6 +20,28 @@ export function RatingTotal({ rating, total }: { rating: number; total?: number 
       <StarIcon className="fill-amber-600 stroke-amber-700" />
       <span className="text-2xl">{rating.toFixed(1)} </span>
       <span className="opacity-65">{total}</span>
+    </div>
+  );
+}
+
+export function RatingSelect({ value, onValueChange }: { value: number; onValueChange: (value: number) => void }) {
+  return (
+    <div className="flex gap-6">
+      {Array.from({ length: 5 }, (_, i) => (
+        <Button
+          // biome-ignore lint/suspicious/noArrayIndexKey: ok
+          key={i}
+          onClick={() => onValueChange(i + 1)}
+          variant="ghost"
+          size="icon-xl"
+        >
+          <StarIcon
+            className={cn('fill-transparent stroke-amber-700 transition-[fill]', {
+              'fill-amber-600': i < value,
+            })}
+          />
+        </Button>
+      ))}
     </div>
   );
 }
